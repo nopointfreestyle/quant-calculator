a5cc3791e46ffe347c2bccf8ffe87b8410e2263f
nopointfreestyle <nopointfreestyle@gmail.com>
2025-10-21 12:40:34 +0500

    fix font-size

--- src/views/calculators/FormBox.vue
+++ src/views/calculators/FormBox.vue
@@ -32,13 +32,13 @@
     <section class="grid gap-2.5 h-fit">
         <section class="bg-white w-full max-w-[390px] p-5 rounded-2xl border-1 border-[#E2E8F0] flex flex-col h-fit gap-3">
             <div class="flex flex-col gap-1.5" v-for="form in formula.forms">
-                <span class="color-subtitle-100 font-bold text-[1.25rem]">{{ form.title }}</span>
+                <span class="color-subtitle-100 font-[550] text-[1rem]">{{ form.title }}</span>
                 <div class="grid grid-cols-1 gap-2.5 lg:grid-cols-2">
                     <div v-for="box in form.boxes" class="flex w-full">
                         <div v-if="box instanceof InputBoxForm" class="flex w-full items-center gap-1.5 bg-white border-1 border-[#cad5e2] pl-3 pr-3 pt-2 pb-2 rounded-md">
                             <span class="text-[16px] font-medium color-subtitle-100">{{box.icon }}</span>
                             <input 
-                                class="w-full text-[18px] placeholder:text-[#90a1b9] color-title-100 font-medium outline-none border-none" 
+                                class="w-full text-[14px] placeholder:text-[#90a1b9] color-title-100 font-medium outline-none border-none" 
                                 v-model="answers[box.type]"
                                 :placeholder="box.placeholder" 
                                 :type="box.inputType" 
@@ -49,7 +49,7 @@
             </div>
 
             <button 
-                class="w-fit bg-main-100 color-white text-[1.25rem] font-[500] pl-3 pr-3 pt-2 pb-2 rounded-md outline-none hover:opacity-75 transition-[opacity] cursor-pointer"
+                class="w-fit bg-main-100 color-white text-[1rem] font-[500] pl-3 pr-3 pt-2 pb-2 rounded-md outline-none hover:opacity-75 transition-[opacity] cursor-pointer"
                 @click="onMake">
                 Вычислить
             </button>
